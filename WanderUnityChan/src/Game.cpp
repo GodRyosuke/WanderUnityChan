1951775f0dc2a1d000f66b0498958be416cb1a49
GodRyosuke <ienvs0828@outlook.jp>
2023-09-14 07:16:22 +0900

    fixed

--- WanderUnityChan/src/Game.cpp
+++ WanderUnityChan/src/Game.cpp
@@ -273,7 +273,7 @@ bool Game::LoadData()
 	// Load Models
     Actor* a = nullptr;
     // UnityChan Loader改良版
-    a = new ActorUnityChan(this);
+    //a = new ActorUnityChan(this);
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
